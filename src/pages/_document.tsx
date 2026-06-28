517580fa2927834f21cdc20e820d687a9fa3b246
irina.nikulochkina <irina.nikulochkina@redcollar.ru>
2026-06-28 11:26:15 +0300

    feat(blablabla-6): csp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,37 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@400;500&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
+class MyDocument extends Document<{ nonce?: string }> {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const nonce = ctx.req?.headers['x-nonce'];
+
+    return {
+      ...initialProps,
+      nonce,
+    };
+  }
+
+  render() {
+    const nonce = (this.props as any).nonce;
+
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@400;500&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
         <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+        <NextScript nonce={nonce} />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
